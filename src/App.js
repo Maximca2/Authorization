c245ad9b9a5590fd31939bae8a817283655e92b0
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-12 18:25:46 +0300

    add check if user is in Account

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import CreateAccount from './pages/CreateAccount/CreateAccount';
 import LogInPage from './pages/LogIn/LogInPage';
 import UserPage from './pages/CurUserPage/UserPage';
 import RestorePassword from './pages/RestorePassword/RestorePassword';
-
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isUser, setIsUser] = useState(false)
+  const userInAccount = useSelector(state => state.toolkit.userInAccount);
 
+  useEffect(() => {
+    if (!userInAccount) {
+      setIsUser(false)
+    }
+    if(userInAccount){
+      setIsUser(true)
+    }
+    
+  },[userInAccount])
+  
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<LogInPage />} />
-        <Route path="*" element={<LogInPage />} />
+        <Route path='*' element={!isUser ? <UserPage /> : <LogInPage />} />
         <Route path="/userpage/:name" element={<UserPage />} />
         <Route path="/logInPage" element={<LogInPage />} />
         <Route path="/createAccount" element={<CreateAccount />} />
