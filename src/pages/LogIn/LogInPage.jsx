1526e91cc8df0e4f5ca196b723edfe1a5fbe5691
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-12 18:28:07 +0300

    add touste and some logic to check if account exist

--- src/pages/LogIn/LogInPage.jsx
+++ src/pages/LogIn/LogInPage.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 
 import { checkUser } from "../../redux/store/usersReducer";
+
+import "react-toastify/dist/ReactToastify.css";
 
 import style from "../LogIn/LogIn.module.scss";
 
@@ -10,77 +13,101 @@ const LogInPage = () => {
   const dispatch = useDispatch();
   const [dataToLogIn, setdataToLogIn] = useState({});
   const [canGoToUSerPage, setcanGoToUSerPage] = useState(true);
+  const [wrongValue, setWrongValue] = useState(true);
 
   const userExist = useSelector((state) => state.toolkit.userExist);
   const database = useSelector((state) => state.toolkit.database);
   const user = useSelector((state) => state.toolkit.user);
 
+  useEffect(() => {
+    if (userExist) {
+      setWrongValue(true);
+      showMessageSuccess();
+      setcanGoToUSerPage(true);
+    }
+  }, [userExist]);
+  const showMessageError = () => {
+    toast.error("Такого акаунту немає!", {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+  };
+
+  function showMessageSuccess() {
+    toast.success("Ви успішно пройшли ідентифікацію!", {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+  }
+
   function checkIfAccountExist(obj) {
     if (obj.nicName || !obj.password) {
       dispatch(checkUser(obj));
     }
+
     setcanGoToUSerPage(false);
+    setWrongValue(false);
+
+    if (!canGoToUSerPage) {
+      showMessageError();
+      setcanGoToUSerPage(true);
+    }
   }
-  
   return (
     <div className={style.box}>
-      {userExist ? (
-        <div className={style.enterLogInAndPassword}>
-          Введіть логін і пароль
-        </div>
-      ) : (
-        <div className={style.curUserisntExist}>такого користувача немає</div>
-      )}
-      <button
-        className={style.checkBtn}
-        onClick={() => checkIfAccountExist(dataToLogIn, database)}
-      >
-        Провірка акаунту
-      </button>
-      <form>
-        <input
-          type="input"
-          className={
-            !userExist ? style.inputIsNotCorrect : style.inputisCorrect
-          }
-          placeholder="Нік нейм"
-          name="name"
-          id="nic-name"
-          onChange={(e) => {
-            setdataToLogIn({ ...dataToLogIn, nicName: e.target.value });
-          }}
-        />
-        <input
-          type="password"
-          autoComplete="section-blue shipping address-level2"
-          className={
-            !userExist ? style.inputIsNotCorrect : style.inputisCorrect
-          }
-          placeholder="Пароль"
-          id="password"
-          onChange={(e) => {
-            setdataToLogIn({ ...dataToLogIn, password: e.target.value });
-          }}
-        />
-      </form>
-      {!userExist ? null : (
+      <div className={style.textLogIn}>
+        <h1>Log in</h1>
+      </div>
+      <ToastContainer />
+      <div className={style.boxForInp}>
+        <form className={style.form}>
+          <input
+            type="input"
+            className={
+              !wrongValue ? style.inputIsNotCorrect : style.inputisCorrect
+            }
+            placeholder="Нік нейм"
+            name="name"
+            id="nic-name"
+            onChange={(e) => {
+              setdataToLogIn({ ...dataToLogIn, nicName: e.target.value });
+            }}
+          />
+          <input
+            type="password"
+            autoComplete="section-blue shipping address-level2"
+            className={
+              !wrongValue ? style.inputIsNotCorrect : style.inputisCorrect
+            }
+            placeholder="Пароль"
+            id="password"
+            onChange={(e) => {
+              setdataToLogIn({ ...dataToLogIn, password: e.target.value });
+            }}
+          />
+        </form>
+      </div>
+      <div className={style.boxForBtn}>
         <NavLink
+          onClick={() => checkIfAccountExist(dataToLogIn, database)}
           className={style.btnLogIn}
           to={
-            canGoToUSerPage
+            !userExist
               ? "/"
               : `/userpage/${user?.name === undefined ? 1 : user?.name}`
           }
         >
-          Log in
+          {!userExist ? "провірка акаунту" : "Log in"}
         </NavLink>
-      )}
-      <div className={style.haventAccount}>
-        немаєте акаунту?
       </div>
-      
-      <NavLink className={style.toCreateAccount} to="/createAccount">Створити акаунт</NavLink>
-      <NavLink className={style.toRestorePassword} to="/restorePassword">Забули пароль?</NavLink>
+
+      <div className={style.haventAccount}>немаєте акаунту?</div>
+      <div className={style.boxForButtons}>
+        <NavLink className={style.toCreateAccount} to="/createAccount">
+          Створити акаунт
+        </NavLink>
+        <NavLink className={style.toRestorePassword} to="/restorePassword">
+          Забули пароль?
+        </NavLink>
+      </div>
     </div>
   );
 };
