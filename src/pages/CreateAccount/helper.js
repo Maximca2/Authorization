a143f83d90de6efe3c612ee43c72acf73355c355
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-14 19:26:05 +0300

    clean code in helper

--- src/pages/CreateAccount/helper.js
+++ src/pages/CreateAccount/helper.js
@@ -1,12 +1,10 @@
 export function isObject(value) {
     let valued = false;
-    Object.values(value).forEach(it=>it.length<=2 ? valued : valued=true)
-
-   if(Object.values(value).length<4 ||valued===false){
-    return false
-   }
-   return true
-
+    Object.values(value).forEach(it => it.length <= 2 ? valued : valued = true)
+    if (Object.values(value).length < 4 || valued === false) {
+        return false
+    }
+    return true
 
 }
-  
+
