31796f659395c3a3ca11c9cb6a2d28c7b769d040
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-14 19:24:56 +0300

    add logic to check if user in account

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 
 import CreateAccount from './pages/CreateAccount/CreateAccount';
 import LogInPage from './pages/LogIn/LogInPage';
 import UserPage from './pages/CurUserPage/UserPage';
+import MainPage from './pages/MainPage/MainPage';
 import RestorePassword from './pages/RestorePassword/RestorePassword';
+
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
-  const [isUser, setIsUser] = useState(false)
-  const userInAccount = useSelector(state => state.toolkit.userInAccount);
-
-  useEffect(() => {
-    if (!userInAccount) {
-      setIsUser(false)
-    }
-    if(userInAccount){
-      setIsUser(true)
-    }
-    
-  },[userInAccount])
+  const userInAccount = useSelector((state) => state.toolkit.userInAccount);
+  const [userInPage,setUserInPage] = useState(true)
   
+  useEffect(()=>{
+    if (localStorage.getItem('CUR_USER_TOKEN') === '[]') {
+      setUserInPage(false)
+  }else{
+    setTimeout(()=>{
+      setUserInPage(true)
+    },2000)
+      
+  }
+  },[userInAccount])
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<LogInPage />} />
-        <Route path='*' element={!isUser ? <UserPage /> : <LogInPage />} />
-        <Route path="/userpage/:name" element={<UserPage />} />
+        <Route path="/" element={userInPage?<UserPage />: <MainPage />} />
+        <Route path="*" element={userInPage?<UserPage />: <MainPage />} />
+        <Route path="/" element={<UserPage />} />
         <Route path="/logInPage" element={<LogInPage />} />
         <Route path="/createAccount" element={<CreateAccount />} />
         <Route path="/restorePassword" element={<RestorePassword />} />
