3e089b959927e3127cb494ff192b986852d4d5b7
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-18 17:27:32 +0300

    add some logic

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "../MainPage/mainPage.module.scss";
 import { NavLink } from "react-router-dom";
-
+import { setLogIn } from "../../redux/store/usersReducer";
+import { useDispatch } from "react-redux";
 const MainPage = () => {
+  
+  const dispatch = useDispatch();
+
+  const setLog = ()=>{
+    dispatch(setLogIn(false))
+  }
   return (
     <div className={style.boxFor}>
       <div className={style.boxbtnHi}>
         <div className={style.messageHi}>Вітаю!</div>
-        <NavLink to='/logInPage' className={style.btnHi}>Увійти</NavLink>
+        
+        <NavLink to='/logIn' onClick={setLog()}  className={style.btnHi}>Увійти</NavLink>
       </div>
     </div>
   );
