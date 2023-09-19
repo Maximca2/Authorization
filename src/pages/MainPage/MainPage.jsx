2c33a2230121923349bcda8aef758b501d41ce81
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-19 14:13:11 +0300

    clean code

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import style from "../MainPage/mainPage.module.scss";
 import { NavLink } from "react-router-dom";
-import { setLogIn } from "../../redux/store/usersReducer";
-import { useDispatch } from "react-redux";
+
+import style from "../MainPage/mainPage.module.scss";
+
 const MainPage = () => {
   
-  const dispatch = useDispatch();
-
-  const setLog = ()=>{
-    dispatch(setLogIn(false))
-  }
   return (
     <div className={style.boxFor}>
       <div className={style.boxbtnHi}>
         <div className={style.messageHi}>Вітаю!</div>
-        <NavLink to='/logIn' onClick={setLog}  className={style.btnHi}>Увійти</NavLink>
+        <NavLink to='/logInPage'   className={style.btnHi}>Увійти</NavLink>
       </div>
     </div>
   );
