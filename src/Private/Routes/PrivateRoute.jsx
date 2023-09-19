995061dfdac53cab57ae7b4fc699af3bd977a4f5
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-19 14:13:56 +0300

    add folder to PrivateRoute

---
+++ src/Private/Routes/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import MainPage from "../../pages/MainPage/MainPage";
+import { CUR_USER_TOKEN } from "../../redux/store/usersReducer";
+import InfoPage from "../../pages/InfoPage/InfoPage";
+
+const Private = () => {
+  const token = localStorage.getItem(CUR_USER_TOKEN);
+
+  if (!token) {
+    return <MainPage />;
+  }
+  if (token === "[]") {
+    return <MainPage />;
+  }
+  return <InfoPage />;
+};
+export default Private;
