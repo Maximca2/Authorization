86b9eb957065761d2db744cd66300672fca1f3fb
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-19 16:25:46 +0300

    some fix

--- src/Komponents/PrivateRoutes/Routes/PrivateRoute.jsx
+++ src/Komponents/PrivateRoutes/Routes/PrivateRoute.jsx
@@ -1,16 +1,21 @@
 import MainPage from "../../../pages/MainPage/MainPage";
 import { CUR_USER_TOKEN } from "../../../redux/store/usersReducer";
 import InfoPage from "../../../pages/InfoPage/InfoPage";
+import { useSelector } from "react-redux";
 
 const Private = () => {
   const token = localStorage.getItem(CUR_USER_TOKEN);
-
-  if (!token) {
+  const userInAccount = useSelector((state) => state.toolkit.userInAccount);
+  console.log(userInAccount)
+  if (!token|| !userInAccount) {
     return <MainPage />;
   }
-  if (token === "[]") {
-    return <MainPage />;
+  if(token===' '){
+    return <MainPage/>
   }
-  return <InfoPage />;
+  else{
+    return <InfoPage />;
+  }
+  
 };
 export default Private;
