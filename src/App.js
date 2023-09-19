0026340d6357663c53776c9183cf598be7b74c00
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-19 15:52:46 +0300

    fix some bugs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 import CreateAccount from './pages/CreateAccount/CreateAccount';
 import LogInPage from './pages/LogIn/LogInPage';
 import UserPage from './pages/InfoPage/InfoPage';
 import RestorePassword from './pages/RestorePassword/RestorePassword';
-import Private from './Private/Routes/PrivateRoute';
+import Private from './Komponents/PrivateRoutes/Routes/PrivateRoute';
+
+import { CUR_USER_TOKEN } from './redux/store/usersReducer';
+
 import './App.css';
-import { useSelector } from "react-redux";
 
 function App() {
-  const token = localStorage.getItem('CUR_USER_TOKEN');
+  const token = localStorage.getItem(CUR_USER_TOKEN);
   const userInAccount = useSelector((state) => state.toolkit.userInAccount);
 
   return (
@@ -18,9 +21,9 @@ function App() {
         <Route element={<Private />}>
           <Route path='/' element={<UserPage />} />
         </Route>
-        <Route path='/logInPage' element={token === '[]' || token === null || userInAccount ? <LogInPage /> : <Navigate to='/' />} />
-        <Route path='/createAccount' element={token === '[]' || token === null ? <CreateAccount /> : <Navigate to='/' />} />
-        <Route path='/restorePassword' element={token === '[]' || token === null ? <RestorePassword /> : <Navigate to='/' />} />
+        <Route path='/logInPage' element={token === '' || token === null || userInAccount ? <LogInPage /> : <Navigate to='/' />} />
+        <Route path='/createAccount' element={token === '' || token === null ? <CreateAccount /> : <Navigate to='/' />} />
+        <Route path='/restorePassword' element={token === '' || token === null ? <RestorePassword /> : <Navigate to='/' />} />
       </Routes>
     </div>
   );
