bbddeba112b90c0d21071f23ba72fa11373f9923
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-25 08:37:12 +0300

    clean code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { Routes, Route, Navigate} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from "react-redux";
 
 import CreateAccount from './pages/CreateAccount/CreateAccount';
 import LogInPage from './pages/LogIn/LogInPage';
 
 import RestorePassword from './pages/RestorePassword/RestorePassword';
-import Private from './Komponents/PrivateRoutes/Routes/PrivateRoute';
+import Private from './Components/PrivateRoutes/Routes/PrivateRoute';
 
 import MainPage from './pages/MainPage/MainPage';
 import InfoPage from './pages/InfoPage/InfoPage';
@@ -13,21 +13,20 @@ import InfoPage from './pages/InfoPage/InfoPage';
 import './App.css';
 
 function App() {
-  
-  const token = useSelector((state) => state.toolkit.curUserToken );
+
+  const token = useSelector((state) => state.toolkit.curUserToken);
 
   return (
     <div className="app">
       <Routes>
         <Route element={<Private>
-          <InfoPage/>
+          <InfoPage />
         </Private>}>
         </Route>
-        <Route path='/maipage' element={<MainPage />} />
-        <Route path='/' element={token?<InfoPage />:<MainPage/>} />
-        <Route path='/logInPage' element={token?<Navigate to={'/'}/> :<LogInPage/>} />
-        <Route path='/createAccount' element={token?<Navigate to={'/'}/> :<CreateAccount /> } />
-        <Route path='/restorePassword' element={token?<Navigate to={'/'}/> :<RestorePassword />} />
+        <Route path='/' element={token ? <InfoPage /> : <MainPage />} />
+        <Route path="/logIn" element={token ? <Navigate to='/' /> : <LogInPage />} />
+        <Route path='/createAccount' element={token ? <Navigate to='/' /> : <CreateAccount />} />
+        <Route path='/restorePassword' element={token ? <Navigate to='/' /> : <RestorePassword />} />
       </Routes>
     </div>
   );
