6ae5228591be4a68573e181b2703a14f2b152fc0
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-25 08:38:55 +0300

    clean code

---
+++ src/Components/PrivateRoutes/Routes/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import MainPage from "../../../pages/MainPage/MainPage";
+
+import { CUR_USER_TOKEN } from "../../../redux/store/usersReducer";
+
+import { Navigate } from "react-router";
+
+
+const Private = () => {
+
+  const token = localStorage.getItem(CUR_USER_TOKEN);
+  
+  return token ? <Navigate to="/" replace={true} /> : <MainPage />;
+};
+
+export default Private;
