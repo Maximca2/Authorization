7c96de595fb31fe0e24115d8202314171a1cb631
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-27 16:47:43 +0300

    add some onclick

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,14 +2,12 @@ import { NavLink } from "react-router-dom";
 
 import "./style.scss";
 
-function Button({ style, value, to }) {
+function Button({ style, value, to ,takeAccess}) {
   if (to) {
     return (
-      <button  className={`button ${style}`}>
-        <NavLink className="text-none" to={to}>
-          {value}
-        </NavLink>
-      </button>
+      <NavLink onClick={takeAccess}  className={`button ${style}`} to={to}>
+        {value}
+      </NavLink>
     );
   }
   return <button className={`button ${style}`}>{value}</button>;
