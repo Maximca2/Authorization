f4ce816ac2017a0aff87107c245951c8bf899c62
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-28 15:17:15 +0300

    renew all onClicks

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -2,15 +2,15 @@ import { NavLink } from "react-router-dom";
 
 import "./style.scss";
 
-function Button({ style, value, to ,takeAccess}) {
+function Button({ style, value, to ,onClick}) {
   if (to) {
     return (
-      <NavLink onClick={takeAccess}  className={`button ${style}`} to={to}>
+      <NavLink onClick={onClick}  className={`button ${style}`} to={to}>
         {value}
       </NavLink>
     );
   }
-  return <button className={`button ${style}`}>{value}</button>;
+  return <button onClick={onClick} className={`button ${style}`}>{value}</button>;
 }
 
 export default Button;
