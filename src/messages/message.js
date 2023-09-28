ad9a040be483a2e8e60007d91e1f3e2fe0d3d4a1
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-28 15:24:44 +0300

    clean code and add some messages

---
+++ src/messages/message.js
@@ -0,0 +1,5 @@
+export const accountIsntExistOrInputValueIsEmpty = 'Такого акаунту немає aбо ви не заповнили поля!';
+export const successToRestorePassword = 'Ви успішно поміняли пароль!';
+export const successCreateAccount = 'Вітаю ви створили акаунт!';
+export const accountAlreadyExist = 'акаунт вже існує';
+export const fillInputs = 'заповніть поля!';
