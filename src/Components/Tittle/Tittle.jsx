670532796d500e1d6cd5574c6c1216547eeed796
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-09-28 15:20:15 +0300

    add some props to will mutable Tittle and remove Toast Component

--- src/Components/Tittle/Tittle.jsx
+++ src/Components/Tittle/Tittle.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Tittle = ({ value, style }) => (
-  <div className={style}>
-    <h1>{value}</h1>
+const Tittle = ({ value, style ,wrapperProps = {},tittleProps={}}) => (
+  <div  className={style} {...wrapperProps} >
+    <h1 {...tittleProps}>{value}</h1>
   </div>
 );
 
