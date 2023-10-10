82711c7a288bcacf63abd0f14fa83e07d1233c78
Maxonchic <maksym.panasiuk.ki.2020@lpnu.ua>
2023-10-10 20:05:28 +0300

    remove any from useState and add second param to my helpers funct

--- src/pages/CreateAccount/CreateAccount.tsx
+++ src/pages/CreateAccount/CreateAccount.tsx
@@ -1,64 +1,67 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import clsx from "clsx";
 import { ToastContainer } from "react-toastify";
 //helpers
-import {
-  toastError,
-  toastSuccess,
-  toastInputIsEmpty,
-} from "../../helpers";
+import { toastError, toastSuccess, toastInputIsEmpty } from "../../helpers";
 import { checkAllValueInObject } from "../../helpers/helper";
 //Components
 import Tittle from "../../Components/Tittle";
 import Input from "../../Components/Input";
 import Button from "../../Components/Button";
 //Reducer
-import {createAccountUser } from "../../redux/store/usersReducer";
+import { createAccountUser } from "../../redux/store/usersReducer";
 //interface
-import {CurAccountData} from "../../interface/interface";
+import { CurAccountData } from "../../interface/interface";
 //hooks
 import { AppSelector } from "../../hooks/hooks";
 //message
 import { successCreateAccount, accountAlreadyExist } from "../../messages";
 import { fillInputs } from "../../messages";
+//options
+import { options } from "../../options/options";
 //style
 import "react-toastify/dist/ReactToastify.css";
 import style from "./style.module.scss";
 
-
-export function checkIfCurrentAccountExist(value:boolean) {
-  
+export function checkIfCurrentAccountExist(value: boolean) {
   if (!value) {
-    return toastError(accountAlreadyExist);
+    return toastError(accountAlreadyExist, options);
   }
   setTimeout(() => {
-    return toastSuccess(successCreateAccount);
+    return toastSuccess(successCreateAccount, options);
   }, 500);
 }
 
 const CreateAccount = () => {
   const dispatch = useDispatch();
-  const [dataToLogIn, setDataToLogIn] = useState<CurAccountData|any>({});
+  const [dataToLogIn, setDataToLogIn] = useState<CurAccountData>({
+    lastName: "",
+    name: "",
+    password: "",
+    nicName: "",
+    token: "",
+  });
   const [inputIsEmpty, setInputIsEmpty] = useState(true);
   const [navigate, setNavigate] = useState(false);
 
   const canLogIn = AppSelector((state) => state.toolkit.canLogIn);
 
-  const createAccount = (obj:CurAccountData) => {
+  const createAccount = (obj: CurAccountData) => {
     if (checkAllValueInObject(obj)) {
-      const uniqueToken:string = uuidv4();
-      const objUniqueToken:{curAccountData:CurAccountData,token:string} = {
-        token: uniqueToken,
-        curAccountData: obj,
-      };
+      const uniqueToken: string = uuidv4();
+      const objUniqueToken: { curAccountData: CurAccountData; token: string } =
+        {
+          token: uniqueToken,
+          curAccountData: obj,
+        };
       dispatch(createAccountUser(objUniqueToken));
     } else {
-      setInputIsEmpty(false)
-      toastInputIsEmpty(fillInputs)
+      setInputIsEmpty(false);
+      toastInputIsEmpty(fillInputs, options);
     }
   };
 
@@ -75,7 +78,12 @@ const CreateAccount = () => {
 
   return (
     <div className={style.box}>
-      <Tittle wrapperProps={null} tittleProps={null}  styles={style.createAcc} value="Create Account" />
+      <Tittle
+        wrapperProps={null}
+        tittleProps={null}
+        styles={style.createAcc}
+        value="Create Account"
+      />
       <div className={style.inpBox}>
         <Input
           type="input"
@@ -85,7 +93,9 @@ const CreateAccount = () => {
           placeholder="Name"
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
             setDataToLogIn({ ...dataToLogIn, name: e.target.value });
-          } }  id={""}        />
+          }}
+          id={""}
+        />
         <Input
           type="input"
           styles={clsx({
@@ -94,7 +104,9 @@ const CreateAccount = () => {
           placeholder="LastName"
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
             setDataToLogIn({ ...dataToLogIn, lastName: e.target.value });
-          } } id={""}        />
+          }}
+          id={""}
+        />
         <Input
           type="input"
           styles={clsx({
@@ -103,7 +115,9 @@ const CreateAccount = () => {
           placeholder="Nic Name"
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
             setDataToLogIn({ ...dataToLogIn, nicName: e.target.value });
-          } } id={""}        />
+          }}
+          id={""}
+        />
         <Input
           type="password"
           styles={clsx({
@@ -112,17 +126,24 @@ const CreateAccount = () => {
           placeholder="Password"
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
             setDataToLogIn({ ...dataToLogIn, password: e.target.value });
-          } }  id={""}        />
+          }}
+          id={""}
+        />
       </div>
-      <ToastContainer/>
+      <ToastContainer />
       <Button
         to={null}
         styles={style.createAccountBtn}
         onClick={() => createAccount(dataToLogIn)}
-        value='Зареєструвати'
+        value="Зареєструвати"
       />
       {navigate ? <Navigate to="/logIn" /> : null}
-      <Button styles={style.LogInBtn} to="/logIn" value={"Log in"} onClick={null}/>
+      <Button
+        styles={style.LogInBtn}
+        to="/logIn"
+        value={"Log in"}
+        onClick={null}
+      />
     </div>
   );
 };
